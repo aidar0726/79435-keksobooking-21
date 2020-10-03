11eaf19b0eb02e7ed0f830c711c7fb3b68afa28d
Irina Zakharova <letter.zakharova@ya.ru>
2020-10-03 08:38:37 +0300

    'edits'

--- js/main.js
+++ js/main.js
@@ -10,13 +10,13 @@ function getArrayCart () {
     let arrayType = ["palace","flat","house","bungalow"];
     let arrayCheckin = ["12:00","13:00","14:00"];
     let arrayFeatures = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-    let ArrayPhotosUrl = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", 
-                          "http://o0.github.io/assets/images/tokyo/hotel2.jpg", 
+    let ArrayPhotosUrl = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg",
+                          "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
                           "http://o0.github.io/assets/images/tokyo/hotel3.jpg"]
 
     for (let i = 0; i < 8; i++) {
         let Cart = {author: {}, offer: {}, location: {}};
-        Cart.author.avatar = "img/avatars/user"+ "0" + i  +".png";
+        Cart.author.avatar = "img/avatars/user"+ "0" + (i + 1)  +".png";
         Cart.offer.title = "Заголовок";
         Cart.offer.adress = "600, 350";
         Cart.offer.price = "350";
@@ -29,7 +29,7 @@ function getArrayCart () {
         Cart.offer.description = "Описание";
         Cart.offer.photos = ArrayPhotosUrl;
         Cart.location.x = 20;
-        Cart.location.y = 630;
+        Cart.location.y = Math.floor(Math.random()*30);
 
         ArrayCart.push(Cart);// заполняем массив
     }
@@ -38,40 +38,31 @@ function getArrayCart () {
 
 let arrayCart = getArrayCart ();
 
-//console.log(arrayCart);
-
-//временно удаляем класс 
+//временно удаляем класс
 let map = document.querySelector('.map');
-console.log(map);
 map.classList.remove('map--faded');
-
 
 //нахождение шаблона с метками
 let pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-//let pinElement = pinTemplate.cloneNode(true);
 
-//функция для заполнения данными шаблона
-function fillingTemplateData (arrayData) {
-    let pins = document.createDocumentFragment();
-    
-    for (var i = 1; i < arrayData.length; i++) {
-        let pinElement = pinTemplate.cloneNode(true);
-        pinElement.querySelector('img').src = arrayData[i].author.avatar;
-        pinElement.querySelector('img').alt = arrayData[i].offer.title;
-        pinElement.style = "left:"+ (arrayData[i].location.x + pinWidth) +"px;"+ "top:"+ (arrayData[i].location.y + pinHeight) +"px;"
-        
-        pins.appendChild(pinElement);
-      }
-
-      return pins;
+//функция для создания пина
+function createPin(pinData) {
+  const pinElement = pinTemplate.cloneNode(true);
+  pinElement.querySelector('img').src = pinData.author.avatar;
+  pinElement.querySelector('img').alt = pinData.offer.title;
+  pinElement.style = "left:"+ (pinData.location.x + pinWidth) +"px;"+ "top:"+ (pinData.location.y + pinHeight) +"px;"
+  return pinElement;
 }
 
 //функция для вставки готовых пинов в разметку
-
 function insertTemplatePin () {
     let mapPinTemplate = document.querySelector('.map__pins');
-    let readyTemplates = fillingTemplateData(arrayCart);
-    mapPinTemplate.appendChild(readyTemplates);
+    let pins = document.createDocumentFragment();
+    for (let i = 0; i < arrayCart.length; i++) {
+      const pin = createPin(arrayCart[i])
+      pins.appendChild(pin);
+    }
+    mapPinTemplate.appendChild(pins);
 }
 
 insertTemplatePin();
